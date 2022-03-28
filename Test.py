c64a75e8b33bf9dc9d4218a90781c6c652e33e87
Legitshadows <la.beltran19@info.uas.edu.mx>
2022-03-28 16:55:46 -0700

    Messed around with index and print stuff in string

--- Test.py
+++ Test.py
@@ -8,3 +8,8 @@ char_age = "30"
 print ("Not much was known about " + char_name + ", ")
 print ("but he sure likes basketball and still plays at " + char_age + ".") """
 
+"""phrase = "Legit Shadows"
+#print (len(phrase)) # This is to get the length of a string in this case "phrase"
+#(phrase[0]) # This is used to find the letter located on the certain index aka [#]
+#print (phrase.index("t")) # Like above, this one is used to find the number (index) in which the letter "letter" is located in
+print (phrase.replace("Legit", "False"))"""
