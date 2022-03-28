131077464694380b20bc5ce61844adf6ef87567c
Legitshadows <la.beltran19@info.uas.edu.mx>
2022-03-28 16:37:35 -0700

    Did some variable testing and changes

---
+++ Test.py
@@ -0,0 +1,10 @@
+""" char_name = "Pablo"
+char_age = "25"   
+print ("There once was a man named " + char_name + ", ")
+print ("he was atleast " + char_age + " years old.")
+
+char_name = "Ramon"
+char_age = "30"
+print ("Not much was known about " + char_name + ", ")
+print ("but he sure likes basketball and still plays at " + char_age + ".") """
+
