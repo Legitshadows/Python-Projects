71b6caccd0f87005fd47cf0cab35966f699e081f
Legitshadows <la.beltran19@info.uas.edu.mx>
2022-03-28 17:12:18 -0700

    Testing out numbers now with variables and funcs

--- Test.py
+++ Test.py
@@ -13,3 +13,7 @@ print ("but he sure likes basketball and still plays at " + char_age + ".") """
 #(phrase[0]) # This is used to find the letter located on the certain index aka [#]
 #print (phrase.index("t")) # Like above, this one is used to find the number (index) in which the letter "letter" is located in
 print (phrase.replace("Legit", "False"))"""
+
+my_num = -10
+#print (str(my_num) + " is my favorite number") # This is used to convert a integer to a string so that you can combine a number in its integer form and another string
+print (abs(my_num))
